9f73b4dc090dda023c65caf1a139e38cf6856432
Raffaele <raffaele.gerosa@cern.ch>
2021-12-07 20:12:09 +0100

    updates for testing AK4

--- utils/nn/model/ParticleNet.py
+++ utils/nn/model/ParticleNet.py
@@ -275,3 +275,40 @@ class ParticleNetTagger(nn.Module):
         features = torch.cat((self.pf_conv(pf_features * pf_mask) * pf_mask, self.sv_conv(sv_features * sv_mask) * sv_mask), dim=2)
         mask = torch.cat((pf_mask, sv_mask), dim=2)
         return self.pn(points, features, mask)
+
+
+class ParticleNetTaggerNoSV(nn.Module):
+
+    def __init__(self,
+                 pf_features_dims,
+                 num_classes,
+                 conv_params=[(7, (32, 32, 32)), (7, (64, 64, 64))],
+                 fc_params=[(128, 0.1)],
+                 use_fusion=True,
+                 use_fts_bn=True,
+                 use_counts=True,
+                 pf_input_dropout=None,
+                 for_inference=False,
+                 **kwargs):
+        super(ParticleNetTagger, self).__init__(**kwargs)
+        self.pf_input_dropout = nn.Dropout(pf_input_dropout) if pf_input_dropout else None
+        self.pf_conv = FeatureConv(pf_features_dims, 32)
+        self.pn = ParticleNet(input_dims=32,
+                              num_classes=num_classes,
+                              conv_params=conv_params,
+                              fc_params=fc_params,
+                              use_fusion=use_fusion,
+                              use_fts_bn=use_fts_bn,
+                              use_counts=use_counts,
+                              for_inference=for_inference)
+
+    def forward(self, pf_points, pf_features, pf_mask):
+        if self.pf_input_dropout:
+            pf_mask = (self.pf_input_dropout(pf_mask) != 0).float()
+            pf_points *= pf_mask
+            pf_features *= pf_mask
+
+        points = torch.cat((pf_points), dim=1)
+        features = torch.cat((self.pf_conv(pf_features * pf_mask) * pf_mask),dim=1)
+        mask = torch.cat((pf_mask), dim=1)
+        return self.pn(points, features, mask)
