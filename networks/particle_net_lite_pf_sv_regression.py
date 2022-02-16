505746d5d746f80a95b188ad13993ff58006d9c4
Raffaele <raffaele.gerosa@cern.ch>
2022-02-16 13:37:39 +0100

    small fixes

--- networks/particle_net_lite_pf_sv_regression.py
+++ networks/particle_net_lite_pf_sv_regression.py
@@ -1,6 +1,8 @@
+import numpy as np
+import math
 import torch
+from torch import Tensor
 from utils.nn.model.ParticleNet import ParticleNetTagger
-
 
 def get_model(data_config, **kwargs):
     conv_params = [
@@ -27,8 +29,8 @@ def get_model(data_config, **kwargs):
     model_info = {
         'input_names':list(data_config.input_names),
         'input_shapes':{k:((1,) + s[1:]) for k, s in data_config.input_shapes.items()},
-        'output_names':['softmax'],
-        'dynamic_axes':{**{k:{0:'N', 2:'n_' + k.split('_')[0]} for k in data_config.input_names}, **{'softmax':{0:'N'}}},
+        'output_names':['output'],
+        'dynamic_axes':{**{k:{0:'N', 2:'n_' + k.split('_')[0]} for k in data_config.input_names}, **{'output':{0:'N'}}},
         }
 
     return model, model_info
